56ff8aab622e8729e69c38136645cac5484a8ac5
Dewen <saryan02@mail.ru>
2026-04-12 21:23:18 +1000

    feat(product): add component product

---
+++ client/src/app/catalog/page.tsx
@@ -0,0 +1,12 @@
+import {ProductCard} from "@/features/product/components/ProductCard/ProductCard";
+
+export default function Catalog() {
+    return (
+        <>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+        </>
+)
+}
---
+++ client/src/features/product/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,7 @@
+export function ProductCard() {
+    return (
+        <div>
+            <text>123</text>
+        </div>
+    )
+}
